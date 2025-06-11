ff6e6d2507493688f40051390141df85efb7e783
Valter-Santos <valtersantosbr3015@gmail.com>
2025-06-11 05:35:35 -0300

    Delete src/main/java/com/seudominio directory

--- src/main/java/com/seudominio/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seudominio.repository;
-
-import com.seudominio.model.Produto;
-
-public interface ProdutoRepository {
-    void save(Produto produto);
-    Produto findById(int id);
-    Produto[] findAll();
-}
--- src/main/java/com/seudominio/service/ProdutoService.java
+++
@@ -1,9 +0,0 @@
-package com.seudominio.service;
-
-import com.seudominio.model.Produto;
-
-public interface ProdutoService {
-    void criarProduto(Produto produto);
-    Produto buscarPorId(int id);
-    Produto[] listarTodos();
-}
