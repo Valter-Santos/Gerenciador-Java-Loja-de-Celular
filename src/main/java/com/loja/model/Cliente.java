d9c2f08543dcc71258fc3206d71c2a3479ce989a
rannikelly03 <rannikelly65@gmail.com>
2025-06-21 11:36:20 -0300

    Update Cliente.java

---
+++ src/main/java/com/loja/model/Cliente.java
@@ -0,0 +1,13 @@
+package com.loja.model;
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() { return nome; }
+    public String getCpf() { return cpf; }
+}
